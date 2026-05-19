c1bdee0ac9c70a1dd6f4af9e0b5ef7014e4ca7a7
EdahPekarich <e.pekarich@gmail.com>
2026-05-19 23:21:20 +0200

    Update api.js

--- frontend2/src/api/api.js
+++ frontend2/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pet-hotel-app.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
