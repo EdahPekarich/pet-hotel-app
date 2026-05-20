d82926e7268c59ad725e34d98537cddae740ffe7
EdahPekarich <e.pekarich@gmail.com>
2026-05-20 22:49:17 +0200

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,26 +1,22 @@
 const jwt = require("jsonwebtoken");
 
+// AUTH CHECK
 const protect = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ message: "No token" });
   }
 
   try {
-    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
-module.exports = (role) => {
-  return (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-    next();
-  };
-};
+
 module.exports = protect;
